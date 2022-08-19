9c11ea7c2133bb411ea3d66d2ca5c66c9a84dd2e
yecheng.123 <yecheng.123@bytedance.com>
2022-08-19 19:56:11 +0800

    info

--- loginservice/src/main/java/com/example/loginservice/LoginserviceApplication.java
+++ loginservice/src/main/java/com/example/loginservice/LoginserviceApplication.java
@@ -2,12 +2,16 @@ package com.example.loginservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(
+	exclude = {DataSourceAutoConfiguration.class}, 
+    scanBasePackages = {"com.example"}
+) 
 public class LoginserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginserviceApplication.class, args);
 	}
-
 }
